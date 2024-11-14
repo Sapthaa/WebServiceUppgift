6eda67887f256bacea4b87abceba1b3f30a3632d
André sapthawisukhon <andre.saptha@hotmail.com>
2024-11-14 01:58:58 +0100

    Created MovieWebClientConfiguration class with WebClient.Builder bean method

---
+++ src/main/java/com/sapthaa/webserviceuppgift/config/MovieWebClientConfiguration.java
@@ -0,0 +1,14 @@
+package com.sapthaa.webserviceuppgift.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class MovieWebClientConfiguration {
+
+    @Bean
+    public WebClient.Builder movieWebClientBuilder() {
+        return WebClient.builder();
+    }
+}
