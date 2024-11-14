9abe2d73fef02850860d8cb8f93e90b47a99e0e8
André sapthawisukhon <andre.saptha@hotmail.com>
2024-11-14 01:08:49 +0100

    Initial commit

---
+++ src/test/java/com/sapthaa/webserviceuppgift/WebServiceUppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapthaa.webserviceuppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceUppgiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
